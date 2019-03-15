b9c0aa83c088ef6090ca466e4d4ff747d5d19e6e
za-yn <yuan.z@u.nus.edu>
2019-03-15 21:03:46 +0800

    fix 7

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'resumeData.json',
+      url:'/about-me/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
